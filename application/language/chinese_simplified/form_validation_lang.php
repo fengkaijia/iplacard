763ce6896476686b1901231ab239d272efc83a06
Kaijia Feng <fengkaijia@gmail.com>
2013-11-19 23:00:17 +0800

    增加 form_validation 自定义语言文件

---
+++ application/language/chinese_simplified/form_validation_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['required'] = '%s不能为空。';
+$lang['isset'] = '%s必须存在。';
+$lang['valid_email'] = '%s必须包含有效的电子邮箱地址。';
+$lang['valid_emails'] = '%s必须包含有效的电子邮箱地址。';
+$lang['valid_url'] = '%s必须包含有效的网址。';
+$lang['valid_ip'] = '%s必须为一个有效的 IP 地址。';
+$lang['min_length'] = '%s至少需要包含 %s 个字符。';
+$lang['max_length'] = '%s不能超过 %s 个字符。';
+$lang['exact_length'] = '%s必须为 %s 个字符。';
+$lang['alpha'] = '%s只允许包含英文字母。';
+$lang['alpha_numeric'] = '%s只允许包含英文字母或数字。';
+$lang['alpha_dash'] = '%s只允许包含英文字母、数字、下划线（_）或破折号（-）。';
+$lang['numeric'] = '%s只允许为数字。';
+$lang['is_numeric'] = '%s只允许为数字。';
+$lang['integer'] = '%s只允许为整数。';
+$lang['matches'] = '%s与%s不相同。';
+$lang['is_natural'] = '%s必须是自然数。';
+$lang['is_natural_no_zero'] = '%s必须是正数。';
+
+/* End of file IP_form_validation_lang.php */
+/* Location: ./application/language/chinese_simplified/IP_form_validation_lang.php */
