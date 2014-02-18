4eb2e3be17497c3b2395208dc72886555dc1d5d7
Kaijia Feng <fengkaijia@gmail.com>
2014-02-18 17:47:49 +0800

    增加注释笔记分类模块方法

--- application/models/note_model.php
+++ application/models/note_model.php
@@ -118,6 +118,97 @@ class Note_model extends CI_Model
 		$this->db->where('id', $id);
 		return $this->db->delete('note');
 	}
+	
+	/**
+	 * 获取注释笔记分类信息
+	 * @param int $id 分类ID
+	 * @param string $part 指定部分
+	 * @return array|string|boolean 信息，如不存在返回FALSE
+	 */
+	function get_category($id, $part = '')
+	{
+		$this->db->where('id', $id);
+		$query = $this->db->get('note_category');
+		
+		//如果无结果
+		if($query->num_rows() == 0)
+			return false;
+		
+		$data = $query->row_array();
+		
+		//返回结果
+		if(empty($part))
+			return $data;
+		return $data[$part];
+	}
+	
+	/**
+	 * 查询符合条件的第一个注释笔记分类ID
+	 * @return int|false 符合查询条件的第一个分类ID，如不存在返回FALSE
+	 */
+	function get_category_id()
+	{
+		$args = func_get_args();
+		array_unshift($args, 'note_category');
+		//将参数传递给get_id方法
+		return call_user_func_array(array($this->sql_model, 'get_id'), $args);
+	}
+	
+	/**
+	 * 查询符合条件的所有注释笔记分类ID
+	 * @return array|false 符合查询条件的所有分类ID，如不存在返回FALSE
+	 */
+	function get_category_ids()
+	{
+		$args = func_get_args();
+		array_unshift($args, 'note_category');
+		//将参数传递给get_ids方法
+		return call_user_func_array(array($this->sql_model, 'get_ids'), $args);
+	}
+	
+	/**
+	 * 编辑/添加注释笔记分类
+	 * @return int 新的分类ID
+	 */
+	function edit_category($data, $id = '')
+	{
+		//新增注释笔记分类
+		if(empty($id))
+		{
+			$this->db->insert('note_category', $data);
+			return $this->db->insert_id();
+		}
+		
+		//更新注释笔记分类
+		$this->db->where('id', $id);
+		return $this->db->update('note_category', $data);
+	}
+	
+	/**
+	 * 添加注释笔记分类
+	 * @return int 新的注释笔记分类ID
+	 */
+	function add_category($name, $type = '')
+	{
+		$data = array(
+			'name' => $name,
+			'type' => $type
+		);
+		
+		//返回新分类ID
+		return $this->edit_category($data);
+	}
+	
+	/**
+	 * 删除注释笔记分类
+	 * @param int $id 分类ID
+	 * @return boolean 是否完成删除
+	 */
+	function delete_category($id)
+	{
+		$this->db->where('id', $id);
+		return $this->db->delete('note_category');
+	}
 }
 
 /* End of file note_model.php */
