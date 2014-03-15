0cb1c43133011985f997bffd493e1a58707b8dcf
Kaijia Feng <fengkaijia@gmail.com>
2014-03-15 23:37:09 +0800

    UI 辅助函数支持选择单双引号输出选择

--- application/helpers/ui_helper.php
+++ application/helpers/ui_helper.php
@@ -5,14 +5,17 @@
  * @param string $icon 图标标识 
  * @param boolean $space 是否增加空间
  * @param boolean $fixed 是否匹配宽度
+ * @param boolean $double_quota 是否使用双引号输出
  * @return string 包含图标的HTML代码
  */
-function icon($icon, $space = true, $fixed = true)
+function icon($icon, $space = true, $fixed = true, $double_quota = true)
 {
 	$fixed = $fixed ? ' fa-fw' : '';
 	$space = $space ? ' ' : '';
 	
-	return "<i class=\"fa fa-{$icon}{$fixed}\"></i>{$space}";
+	if($double_quota)
+		return "<i class=\"fa fa-{$icon}{$fixed}\"></i>{$space}";
+	return "<i class='fa fa-{$icon}{$fixed}'></i>{$space}";
 }
 
 /**
@@ -21,9 +24,10 @@ function icon($icon, $space = true, $fixed = true)
  * @param boolean $check 是否检查代码是否存在
  * @param boolean $space 是否增加空间
  * @param boolean $null_output 是否在旗帜不存在时显示未知旗帜
+ * @param boolean $double_quota 是否使用双引号输出
  * @return string 包含旗帜的HTML代码
  */
-function flag($iso, $check = false, $space = true, $null_output = false)
+function flag($iso, $check = false, $space = true, $null_output = false, $double_quota = true)
 {
 	if($check)
 	{
@@ -43,7 +47,9 @@ function flag($iso, $check = false, $space = true, $null_output = false)
 	
 	$space = $space ? ' ' : '';
 	
-	return "<span class=\"flag {$iso}\"></span>{$space}";
+	if($double_quota)
+		return "<span class=\"flag {$iso}\"></span>{$space}";
+	return "<span class='flag {$iso}'></span>{$space}";
 }
 
 /* End of file ui_helper.php */
