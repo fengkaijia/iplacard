1168ffb947c4cc2d20474ddd41e3dcc3e2192235
Kaijia Feng <fengkaijia@gmail.com>
2014-04-01 22:37:34 +0800

    增加新增代表界面显示申请状态页面功能

--- application/controllers/apply.php
+++ application/controllers/apply.php
@@ -24,6 +24,7 @@ class Apply extends CI_Controller
 		$this->load->library('ui', array('side' => 'delegate'));
 		$this->load->model('admin_model');
 		$this->load->model('delegate_model');
+		$this->load->helper('text');
 		$this->load->helper('date');
 		
 		//检查登录情况
@@ -58,6 +59,52 @@ class Apply extends CI_Controller
 			$vars['welcome'] = true;
 		else
 			$vars['welcome'] = false;
+		
+		//状态信息
+		$application_type_function = "_status_{$this->delegate['application_type']}";
+		$status_info = $this->$application_type_function();
+		$vars += $status_info;
+		
+		//席位
+		$this->load->model('seat_model');
+		
+		$seat = array();
+		
+		$sid = $this->seat_model->get_seat_id('delegate', $this->uid);
+		if($sid)
+		{
+			$this->load->model('committee_model');
+			
+			$seat = $this->seat_model->get_seat($sid);
+			$seat['committee'] = $this->committee_model->get_committee($seat['committee']);
+		}
+		
+		$vars['seat'] = $seat;
+		
+		//团队
+		$group = array();
+		if($this->delegate['group'])
+		{
+			$this->load->model('group_model');
+			
+			$group = $this->group_model->get_group($this->delegate['group']);
+		}
+		
+		$vars['group'] = $group;
+		
+		//RSS
+		$this->load->library('feed');
+		
+		$this->feed->set_feed_url(option('feed_url', 'http://iplacard.com/feed/'));
+		
+		$feed_enable = false;
+		if($this->feed->parse())
+		{
+			$feed_enable = true;
+			$vars['feed'] = $this->feed->get_feed(2);
+		}
+		
+		$vars['feed_enable'] = $feed_enable;
 		
 		$vars['delegate'] = $this->delegate;
 		
@@ -261,6 +308,314 @@ class Apply extends CI_Controller
 		}
 		
 		echo json_encode($json);
+	}
+	
+	/**
+	 * 代表申请状态信息
+	 */
+	function _status_delegate()
+	{
+		$status = $this->delegate['status'];
+		
+		$w['signin'] = '提交申请';
+		$w['admit'] = '等待通过审核';
+		$w['interview'] = '等待安排面试';
+		$w['seat'] = '等待分配席位';
+		$w['pay'] = '等待支付会费';
+		$w['lock'] = '等待确认完成';
+		
+		//是否存在二次面试
+		$this->load->model('interview_model');
+		$interviews = $this->interview_model->get_interview_ids('delegate', $this->uid, 'status', 'failed');
+		if($interviews && count($interviews) == 1)
+			$w['interview'] = '等待二次面试';
+		
+		//进度条状态文字
+		switch($status)
+		{
+			case 'locked':
+				$w['lock'] = '申请已完成';
+			case 'payment_received':
+				$w['pay'] = '已支付会费';
+			case 'invoice_issued':
+			case 'seat_assigned':
+				$w['seat'] = '席位已分配';
+			case 'interview_assigned':
+				$w['interview'] = '已分配面试';
+			case 'interview_arranged':
+				$w['interview'] = '已安排面试';
+			case 'interview_completed':
+				$w['interview'] = '面试已通过';
+			case 'review_passed':
+				$w['admit'] = '初审已通过';
+			case 'application_imported':
+				$w['signin'] = '申请已录入';
+				break;
+			case 'review_refused':
+				$w['signin'] = '申请已录入';
+				$w['admit'] = '材料初审未通过';
+				$w['interview'] = NULL;
+				$w['seat'] = NULL;
+				$w['pay'] = NULL;
+				$w['lock'] = NULL;
+				break;
+			case 'moved_to_waiting_list':
+				$w['signin'] = '申请已录入';
+				$w['admit'] = '初审已通过';
+				$w['interview'] = '面试未通过';
+				$w['seat'] = NULL;
+				$w['pay'] = NULL;
+				$w['lock'] = '队列等待中';
+				break;
+		}
+		if($status == 'quitted')
+			$w['lock'] = '已经退会';
+		
+		//进度条状态显示
+		switch($status)
+		{
+			case 'application_imported':
+			case 'review_refused':
+				$current = 'admit';
+				break;
+			case 'review_passed':
+			case 'interview_assigned':
+			case 'interview_arranged':
+			case 'interview_completed':
+				$current = 'interview';
+				break;
+			case 'seat_assigned':
+				$current = 'seat';
+				break;
+			case 'invoice_issued':
+			case 'payment_received':
+				$current = 'pay';
+				break;
+			case 'locked':
+			case 'moved_to_waiting_list':
+				$current = 'lock';
+				break;
+			case 'quitted':
+				$current = 'quit';
+				break;
+		}
+		
+		//生成状态组信息
+		foreach(array('signin', 'admit', 'interview', 'seat', 'pay', 'lock') as $one)
+		{
+			if(!empty($w[$one]))
+			{
+				if($current == $one || ($one == 'lock' && $status == 'quitted'))
+					$wizard[] = array('text' => $w[$one], 'intro' => $this->_status_intro($one), 'current' => true);
+				else
+					$wizard[] = array('text' => $w[$one], 'intro' => $this->_status_intro($one), 'current' => false);
+			}
+		}
+		
+		$return['wizard'] = $wizard;
+		$return['current'] = $current;
+		return $return;
+	}
+	
+	/**
+	 * 观察员申请状态信息
+	 */
+	function _status_observer()
+	{
+		$status = $this->delegate['status'];
+		
+		$w['signin'] = '提交申请';
+		$w['admit'] = '等待通过审核';
+		$w['pay'] = '等待支付会费';
+		$w['lock'] = '等待确认完成';
+		
+		//进度条状态文字
+		switch($status)
+		{
+			case 'locked':
+				$w['lock'] = '申请已完成';
+			case 'payment_received':
+				$w['pay'] = '已支付会费';
+			case 'invoice_issued':
+			case 'review_passed':
+				$w['admit'] = '审核已通过';
+			case 'application_imported':
+				$w['signin'] = '申请已录入';
+				break;
+			case 'review_refused':
+				$w['signin'] = '申请已录入';
+				$w['admit'] = '材料审核未通过';
+				$w['pay'] = NULL;
+				$w['lock'] = NULL;
+				break;
+		}
+		if($status == 'quitted')
+			$w['lock'] = '已经退会';
+		
+		//进度条状态显示
+		switch($status)
+		{
+			case 'application_imported':
+			case 'review_refused':
+				$current = 'admit';
+				break;
+			case 'review_passed':
+			case 'interview_assigned':
+			case 'interview_arranged':
+			case 'interview_completed':
+				$current = 'interview';
+				break;
+			case 'seat_assigned':
+				$current = 'seat';
+				break;
+			case 'invoice_issued':
+			case 'payment_received':
+				$current = 'pay';
+				break;
+			case 'locked':
+			case 'moved_to_waiting_list':
+				$current = 'lock';
+				break;
+			case 'quitted':
+				$current = 'quit';
+				break;
+		}
+		
+		//生成状态组信息
+		foreach(array('signin', 'admit', 'pay', 'lock') as $one)
+		{
+			if(!empty($w[$one]))
+			{
+				if($current == $one || ($one == 'lock' && $status == 'quitted'))
+					$wizard[] = array('text' => $w[$one], 'intro' => $this->_status_intro($one), 'current' => true);
+				else
+					$wizard[] = array('text' => $w[$one], 'intro' => $this->_status_intro($one), 'current' => false);
+			}
+		}
+		
+		$return['wizard'] = $wizard;
+		$return['current'] = $current;
+		return $return;
+	}
+	
+	/**
+	 * 面试官申请状态信息
+	 */
+	function _status_volunteer()
+	{
+		$status = $this->delegate['status'];
+		
+		$w['signin'] = '提交申请';
+		$w['admit'] = '等待通过审核';
+		$w['lock'] = '等待完成';
+		
+		//进度条状态文字
+		switch($status)
+		{
+			case 'locked':
+				$w['lock'] = '申请已完成';
+				$w['admit'] = '审核已通过';
+			case 'application_imported':
+				$w['signin'] = '申请已录入';
+				break;
+			case 'review_refused':
+				$w['signin'] = '申请已录入';
+				$w['admit'] = '材料审核未通过';
+				$w['lock'] = NULL;
+				break;
+		}
+		if($status == 'quitted')
+			$w['lock'] = '已经退会';
+		
+		//进度条状态显示
+		switch($status)
+		{
+			case 'application_imported':
+				$current = 'admit';
+				break;
+			case 'review_refused':
+				$current = 'admit';
+				break;
+			case 'locked':
+				$current = 'lock';
+				break;
+			case 'quitted':
+				$current = 'quit';
+				break;
+		}
+		
+		//生成状态组信息
+		foreach(array('signin', 'admit', 'lock') as $one)
+		{
+			if(!empty($w[$one]))
+			{
+				if($current == $one || ($one == 'lock' && $status == 'quitted'))
+					$wizard[] = array('text' => $w[$one], 'intro' => $this->_status_intro($one), 'current' => true);
+				else
+					$wizard[] = array('text' => $w[$one], 'intro' => $this->_status_intro($one), 'current' => false);
+			}
+		}
+		
+		$return['wizard'] = $wizard;
+		$return['current'] = $current;
+		return $return;
+	}
+	
+	/**
+	 * 申请状态介绍文字
+	 */
+	function _status_intro($status = '')
+	{
+		if(empty($status))
+			$status = $this->delegate['status'];
+		
+		switch($status)
+		{
+			case 'signin':
+				$intro = '<p>您的申请已经成功导入到 iPlacard。</p>';
+				break;
+			case 'application_imported':
+			case 'review_refused':
+			case 'admit':
+				$intro = '<p>您的申请已经成功导入到 iPlacard，我们将在近期内审核您提交的申请材料。审核通过时，将会有邮件通知。在此期间，请核对您的申请材料，如果内容有误，您将可以联系修改部分数据。</p>'
+					. '<p>材料审核通过之后，您将进入下一申请流程。</p>';
+				break;
+			case 'review_passed':
+			case 'interview_assigned':
+			case 'interview_arranged':
+			case 'interview_completed':
+			case 'interview':
+				$intro = '<p>在面试阶段中，我们将会根据您的委员会意向指派一位面试官，他将与您取得联系并且确定面试时间和面试方式。请尽量为面试预留足够的时间，如果您无法在约定的时间进行面试，请联系您的面试官，他将会重新为您安排面试时间。面试通过之后，您将可以收到席位分配结果。</p>'
+					. '<p>如果面试没有通过，您将进入等待队列，当有面试通过的代表退会时，您将可以接替该代表的席位。</p>'
+					. '<p>如果您对面试有任何疑问，请与您的面试官取得联系，同时您可以联系管理员，他将尽快回复您的问题。</p>';
+				break;
+			case 'seat_assigned':
+			case 'seat':
+				$intro = '<p>面试通过之后，面试官将会根据您的面试表现为您分配适合的席位。通常情况下，面试官将在完成面试后立即为您分配合适的席位。</p>'
+					. '<p>席位分配后，您将可以在席位信息页面确认席位；如果您认为分配的席位不适合您，请联系您的面试官，他将可以为您重新分配席位。</p>';
+				break;
+			case 'invoice_issued':
+			case 'payment_received':
+			case 'pay':
+				$intro = '<p>您将可以通过银行转账、网银支付或者邮政汇款完成会费支付，我们将在收到汇款之后确定完成支付。</p>';
+				break;
+			case 'locked':
+			case 'moved_to_waiting_list':
+			case 'lock':
+				if($this->delegate['status'] == 'moved_to_waiting_list')
+					$intro = '<p>您已经移动到等待队列。我们将会通过邮件通知您后续事宜，请频繁检查您的电子邮箱并定期登录 iPlacard 了解更新。</p>';
+				else
+					$intro = '<p>您已经完成申请流程。我们将会通过邮件通知您后续事宜，请频繁检查您的电子邮箱并定期登录 iPlacard 了解更新。</p>';
+				break;
+			case 'quitted':
+			case 'quit':
+				$intro = '<p>您已退会，帐号即将删除，如果有任何疑问，请立即联系管理员。</p>';
+				break;
+			default:
+				$intro = '';
+		}
+		
+		return $intro;
 	}
 }
 
--- application/views/delegate/status.php
+++ application/views/delegate/status.php
@@ -35,4 +35,89 @@ $this->load->view('header');?>
 	?>
 </div><?php } ?>
 
+<div class="row">
+	<div class="col-md-12" align="center">
+		<div class="wizard" style="padding-top: 42px; padding-bottom: 20px;">
+			<?php foreach($wizard as $step => $one)
+			{
+				$attr = 'data-html="1" data-placement="bottom" data-trigger="click" data-original-title="'.$one['text'].'" data-toggle="popover" data-content="'.$one['intro'].'"';
+				if($one['current'])
+					$attr .= ' class="status_intro current"';
+				else
+					$attr .= ' class="status_intro"';
+				
+				$now = $step + 1;
+				$label = $one['current'] ? 'default' : 'primary';
+				$text = "<span class=\"label label-$label\">{$now}</span> {$one['text']}";
+				echo anchor('#', $text, $attr, true);
+			}
+			$this->ui->js('footer', "$('.status_intro').popover();");
+			?>
+		</div>
+	</div>
+</div>
+
+<div class="row" style="padding-top: 42px;">
+	<div class="col-md-4">
+		<div id="ui-news" class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php echo icon('info');?>申请状态</h3>
+			</div>
+			<div class="panel-body flags-16">
+				<?php
+				foreach($wizard as $step => $one)
+				{
+					if($one['current'])
+						echo $one['intro'];
+				} ?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-md-4">
+		<?php if($feed_enable) { ?><div id="ui-news" class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php echo icon('globe');?>会议新闻</h3>
+			</div>
+			<div class="list-group">
+				<?php foreach($feed as $num => $news) { ?><a href="<?php echo $news['link'];?>" target="_blank" class="list-group-item">
+					<h4 class="list-group-item-heading"><?php echo $news['title'];?> <small><?php echo nicetime(strtotime($news['date']));?></small></h4>
+					<div class="list-group-item-text">
+						<?php echo character_limiter(strip_tags($news['description']), 300);?>
+					</div>
+				</a><?php } ?>
+			</div>
+		</div><?php } ?>
+	</div>
+	
+	<div class="col-md-4">
+		<div id="ui-news" class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><?php echo icon('user');?>个人信息</h3>
+			</div>
+			<div class="panel-body flags-16" style="position: relative;">
+				<a class="thumbnail" style="width: 90px; height: 90px; position: absolute; margin-top: 2px;">
+					<?php echo avatar($delegate['id'], 80, 'img');?>
+				</a>
+				<p style="margin-bottom: 4px; margin-left: 104px;"><strong><?php echo $delegate['name'];?></strong></p>
+				<p style="margin-bottom: 4px; margin-left: 104px;"><?php echo $delegate['application_type_text'];?></p>
+				<?php
+				if(!empty($seat))
+				{
+					echo '<p style="margin-bottom: 4px; margin-left: 104px;">'.flag($seat['iso'], true)."{$seat['name']}，{$seat['committee']['name']}委员会</p>";
+				}
+				
+				if(!empty($group))
+				{
+					echo "<p style=\"margin-bottom: 4px; margin-left: 104px;\">{$group['name']}代表团</p>";
+				} ?>
+				<br />
+				<p style="margin-bottom: 4px; margin-left: 104px;"><strong>联系方式</strong> <?php echo anchor('account/settings/home', '更新');?></p>
+				<p style="margin-bottom: 4px; margin-left: 104px;"><?php echo icon('envelope-o').$delegate['email'];?></p>
+				<p style="margin-bottom: 4px; margin-left: 104px;"><?php echo icon('user').$delegate['phone'];?></p>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?php $this->load->view('footer');?>
