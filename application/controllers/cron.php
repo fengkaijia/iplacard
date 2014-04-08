92ec9377762df8afab10a0ddc1ac25ae6884bbe2
Kaijia Feng <fengkaijia@gmail.com>
2014-04-08 10:09:07 +0800

    新增面试提醒功能

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -39,6 +39,7 @@ class Cron extends CI_Controller
 	function hourly()
 	{
 		echo "\nProcessing Cron Hourly.\n\n";
+		$this->_remind_interview();
 	}
 	
 	function daily()
@@ -49,6 +50,156 @@ class Cron extends CI_Controller
 	function weekly()
 	{
 		echo "\nProcessing Cron Weekly.\n\n";
+	}
+	
+	/**
+	 * 发送面试提醒
+	 */
+	private function _remind_interview()
+	{
+		$this->load->model('admin_model');
+		$this->load->model('delegate_model');
+		$this->load->model('interview_model');
+		
+		//获取所有在通知窗口的面试安排
+		$reminders = array();
+		
+		$ids = $this->interview_model->get_interview_ids('status', 'arranged');
+		if($ids)
+		{
+			foreach($ids as $id)
+			{
+				$interview = $this->interview_model->get_interview($id);
+				
+				//面试开始前2小时内通知
+				$schedule = intval($interview['schedule_time']);
+				if($schedule > (time() + 1 * 60 * 60) && $schedule < (time() + 2 * 60 * 60))
+					$reminders[] = $interview;
+			}
+		}
+		
+		if(!empty($reminders))
+		{
+			$this->load->library('email');
+			$this->load->library('parser');
+			
+			$count = count($reminders);
+			echo "{$count} interview(s) to be reminded.\n";
+			
+			foreach($reminders as $reminder)
+			{
+				$sent = true;
+				
+				echo "Processing Inteviewer Reminder ID {$reminder['id']}.\n";
+				
+				$interviewer = $this->user_model->get_user($reminder['interviewer']);
+				$delegate = $this->user_model->get_user($reminder['delegate']);
+				
+				$data = array(
+					'interview_id' => $reminder['id'],
+					'interviewer_name' => $interviewer['name'],
+					'interviewer_email' => $interviewer['email'],
+					'delegate_id' => $delegate['id'],
+					'delegate_name' => $delegate['name'],
+					'delegate_email' => $delegate['email'],
+					'time' => unix_to_human($reminder['schedule_time'])
+				);
+
+				//通知面试官
+				$this->email->to($interviewer['email']);
+				$this->email->subject('iPlacard 面试提醒');
+				$this->email->html($this->parser->parse_string($this->system->option('email_cron_interview_reminder', '您将于 {time} 面试{delegate_name}代表，请预留充足时间并按照约定的方式与代表联系。'), $data, true));
+				
+				if(!$this->email->send())
+				{
+					echo "Failed to send Interview Reminder to {$interviewer['email']}\n";
+					$this->system_model->log('notice_failed', array('id' => $interviewer['id'], 'type' => 'email', 'content' => 'cron_interview_reminder'), 0);
+					$sent = false;
+				}
+				else
+				{
+					echo "Interview Reminder sent to {$interviewer['email']}\n";
+				}
+				
+				$this->email->clear();
+				
+				//短信通知
+				if(option('sms_enabled', false))
+				{
+					$this->load->model('sms_model');
+					$this->load->library('sms');
+					
+					$this->sms->to($interviewer['id']);
+					$this->sms->message($this->parser->parse_string('将于 {time} 面试{delegate_name}代表，请预留充足时间。', $data, true));
+					
+					if(!$this->sms->send())
+					{
+						echo "Failed to send Interview Reminder SMS to {$interviewer['phone']}\n";
+						$this->system_model->log('notice_failed', array('id' => $interviewer['id'], 'type' => 'sms', 'content' => 'cron_interview_reminder'), 0);
+						$sent = false;
+					}
+					else
+					{
+						echo "Interview Reminder SMS sent to {$interviewer['phone']}\n";
+					}
+					
+					$this->sms->clean();
+				}
+				
+				//通知代表
+				if($this->system->option('cron_remind_delegate_interview', true))
+				{
+					$this->email->to($delegate['email']);
+					$this->email->subject('iPlacard 面试提醒');
+					$this->email->html($this->parser->parse_string($this->system->option('email_cron_delegate_interview_reminder', '您与面试官{interviewer_name}的面试将于 {time} 进行，请预留充足时间并按照约定的方式与面试官联系。'), $data, true));
+					
+					if(!$this->email->send())
+					{
+						echo "Failed to send Interview Reminder to {$delegate['email']}.\n";
+						$this->system_model->log('notice_failed', array('id' => $delegate['id'], 'type' => 'email', 'content' => 'cron_interview_reminder'));
+						$sent = false;
+					}
+					else
+					{
+						echo "Interview Reminder sent to {$delegate['email']}.\n";
+					}
+					
+					$this->email->clear();
+					
+					//短信通知
+					if(option('sms_enabled', false))
+					{
+						$this->load->model('sms_model');
+						$this->load->library('sms');
+
+						$this->sms->to($delegate['id']);
+						$this->sms->message($this->parser->parse_string('与{interviewer_name}的面试将在 {time} 进行，请预留充足时间。', $data, true));
+						if(!$this->sms->send())
+						{
+							echo "Failed to send Interview Reminder SMS to {$delegate['phone']}\n";
+							$this->system_model->log('notice_failed', array('id' => $delegate['id'], 'type' => 'sms', 'content' => 'cron_interview_reminder'));
+							$sent = false;
+						}
+						else
+						{
+							echo "Interview Reminder SMS sent to {$delegate['phone']}\n";
+						}
+						
+						$this->sms->clean();
+					}
+				}
+				
+				//记录通知时间
+				if($sent)
+				{
+					$this->system_model->log('cron_interview_reminder_sent', array('inteview' => $reminder['id']), 0);
+				}
+			}
+		}
+		else
+		{
+			echo "No Interview to be reminded.\n";
+		}
 	}
 }
 
