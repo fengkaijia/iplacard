1442cc7af49d19ee775cf9a149f49a3d580d441c
Kaijia Feng <fengkaijia@gmail.com>
2014-04-09 21:31:12 +0800

    修复席位菜单显示错误

--- application/libraries/Ui.php
+++ application/libraries/Ui.php
@@ -497,6 +497,7 @@ class UI
 		
 		if($this->CI->admin_model->capable('administrator'))
 		{
+			$this->add_menu('seat', '席位');
 			$this->add_sub_menu('manage', 'seat', '全部席位', 'document/manage');
 			$this->add_sub_menu('add', 'seat', '添加席位', 'document/edit');
 		}
