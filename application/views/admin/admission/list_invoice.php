b9302cf496bfaf32cf6f320ed2dbc0d0a9b60f5d
Kaijia Feng <fengkaijia@gmail.com>
2014-05-20 10:13:32 +0800

    调整 div 元素行距

--- application/views/admin/admission/list_invoice.php
+++ application/views/admin/admission/list_invoice.php
@@ -1,7 +1,7 @@
 <?php if(count($invoice_unpaid) > 0) { ?><p>代表当前共有 <?php echo $invoice_count;?> 份账单，其中包含 <?php echo count($invoice_unpaid);?> 份未支付账单。</p><?php }
 else { ?><p>代表共有 <?php echo $invoice_count;?> 份账单。</p><?php } ?>
 
-<?php if(count($invoice_unpaid) == 1) { ?><div class="btn-group">
+<?php if(count($invoice_unpaid) == 1) { ?><div class="btn-group" style="margin-bottom: 10.5px;">
 	<a class="btn btn-primary" href="<?php echo base_url("billing/invoice/{$invoice_unpaid[0]}");?>"><?php echo icon('file-text');?>查看账单 #<?php echo $invoice_unpaid[0];?></a>
 	<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
 	<ul class="dropdown-menu">
