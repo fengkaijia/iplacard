46d02826b8bfa7a33e006a60663d156116ede8f8
Kaijia Feng <fengkaijia@gmail.com>
2015-01-09 21:03:08 +0800

    修复“结束资料编辑”按钮文字提示错误

--- application/views/admin/admission/edit_profile.php
+++ application/views/admin/admission/edit_profile.php
@@ -13,5 +13,5 @@
 <div id="profile_edit_close">
 	<p>现在可以单击需要编辑的资料内容并在弹出的提示框中修改。代表资料的变动将同时显示给对应代表。</p>
 	
-	<p><a class="btn btn-success" id="profile_edit_disable" onclick="$('#profile_edit_open').show(); $('#profile_edit_close').hide(); $('.profile_editable').editable('toggleDisabled');"><?php echo icon('check');?>结束代表资料</a></p>
+	<p><a class="btn btn-success" id="profile_edit_disable" onclick="$('#profile_edit_open').show(); $('#profile_edit_close').hide(); $('.profile_editable').editable('toggleDisabled');"><?php echo icon('check');?>结束资料编辑</a></p>
 </div>
