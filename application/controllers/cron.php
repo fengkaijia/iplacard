0f8cfc16b4891ee933fdaa9ef552826cccd33be8
Kaijia Feng <fengkaijia@gmail.com>
2015-07-11 11:47:34 +0800

    更改符号链接下载方式为临时文件下载方式

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -34,7 +34,7 @@ class Cron extends CI_Controller
 	function minutely()
 	{
 		echo "\nProcessing Cron Minutely.\n\n";
-		$this->_cleanup_symlink_download();
+		$this->_cleanup_temp_download();
 		$this->_send_sms();
 	}
 	
@@ -58,9 +58,9 @@ class Cron extends CI_Controller
 	}
 	
 	/**
-	 * 清理过时文件下载符号链接
+	 * 清理过时临时文件下载
 	 */
-	private function _cleanup_symlink_download()
+	private function _cleanup_temp_download()
 	{
 		$this->load->helper('file');
 		
@@ -68,7 +68,7 @@ class Cron extends CI_Controller
 		if($list)
 		{
 			$count = count($list);
-			echo "{$count} symlink(s) to be checked.\n";
+			echo "{$count} temp directory(s) to be checked.\n";
 			
 			foreach($list as $directory)
 			{
@@ -80,11 +80,11 @@ class Cron extends CI_Controller
 					delete_files('./temp/'.IP_INSTANCE_ID.'/download/'.$directory);
 					@rmdir('./temp/'.IP_INSTANCE_ID.'/download/'.$directory);
 					
-					echo "Symlink {$directory} removed.\n";
+					echo "Temp directory {$directory} removed.\n";
 				}
 				else
 				{
-					echo "Symlink {$directory} is unchanged.\n";
+					echo "Temp directory {$directory} is unchanged.\n";
 				}
 			}
 		}
