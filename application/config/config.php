53909564bff34a51c89abda9775eae6b9b959d36
Kaijia Feng <fengkaijia@gmail.com>
2015-11-25 00:45:11 +0800

    更新 CodeIgniter 系统文件目录及子模块配置至版本 2.2.6

--- application/config/config.php
+++ application/config/config.php
@@ -10,8 +10,16 @@
 |
 |	http://example.com/
 |
-| If this is not set then CodeIgniter will guess the protocol, domain and
-| path to your installation.
+| WARNING: You MUST set this value!
+|
+| If it is not set, then CodeIgniter will try guess the protocol and path
+| your installation, but due to security concerns the hostname will be set
+| to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
+| The auto-detection mechanism exists only for convenience during
+| development and MUST NOT be used in production!
+|
+| If you need to allow multiple domains, remember that this file is still
+| a PHP script and you can easily do that on your own.
 |
 */
 $config['base_url']	= IP_SSL ? 'https://'.IP_INSTANCE_DOMAIN.'/' : 'http://'.IP_INSTANCE_DOMAIN.'/';
